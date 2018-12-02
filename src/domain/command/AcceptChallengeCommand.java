b1c13c84c5ee079d9107d410ce35fbe26a82741e
gprattico <gprattico95@gmail.com>
2018-12-02 00:57:52 -0500

    draw a card when the cahllenge is accepted

--- src/domain/command/AcceptChallengeCommand.java
+++ src/domain/command/AcceptChallengeCommand.java
@@ -1,13 +1,19 @@
 package domain.command;
 
+import java.util.ArrayList;
+
 import org.dsrg.soenea.domain.command.CommandException;
 import org.dsrg.soenea.domain.helper.Helper;
 import org.dsrg.soenea.domain.mapper.LostUpdateException;
 
+import domain.card.Card;
+import domain.card.CardInputMapper;
+import domain.card.CardOutputMapper;
 import domain.challenge.Challenge;
 import domain.challenge.ChallengeInputMapper;
 import domain.challenge.ChallengeOutputMapper;
 import domain.challenge.ChallengeStatus;
+import domain.deck.Deck;
 import domain.deck.DeckInputMapper;
 import domain.game.Game;
 import domain.game.GameInputMapper;
@@ -48,7 +54,12 @@ public class AcceptChallengeCommand extends AbstractCommand {
 		 //0 for in progress, 1 for version, challenger id for currentTurn
 		Game game = new Game(GameInputMapper.getMaxGameID(), challenge.getChallenger(), challenge.getChallengee(), 0, 1, challenge.getChallenger());
 		GameOutputMapper.insert(game);
-		//bla bla draw card
+		//draw card
+		//end turn will let players draw afterward
+		Deck deck = DeckInputMapper.findByUserID(challenge.getChallenger());
+		ArrayList<Card> cardList = CardInputMapper.findAll(deck.getId());
+		cardList.get(0).setStatus(1);//drawing the first card is equivalent to setting its status to in the hand (1)
+		CardOutputMapper.update(cardList.get(0));
 		
 		
 		}catch(Exception e) {
